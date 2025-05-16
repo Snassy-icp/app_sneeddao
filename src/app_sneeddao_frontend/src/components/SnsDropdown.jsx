eb0b711483ebd21d5bf7b46c80dd1a907cbe1c10
snassy-icp <snassy.icp@gmail.com>
2025-05-16 16:05:08 +0200

    header new dropdown

--- src/app_sneeddao_frontend/src/components/SnsDropdown.jsx
+++ src/app_sneeddao_frontend/src/components/SnsDropdown.jsx
@@ -132,6 +132,7 @@ function SnsDropdown() {
                     marginTop: '4px',
                     maxHeight: '300px',
                     overflowY: 'auto',
+                    overflowX: 'hidden',
                     zIndex: 1000,
                     boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
                     minWidth: '250px'
