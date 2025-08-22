1fd2fc1b37a1e5b52f34ea037002103e8d60893c
snassy-icp <snassy-icp@gmail.com>
2025-08-22 20:26:55 +0200

    Me submenu messages then posts

--- src/app_sneeddao_frontend/src/components/Header.jsx
+++ src/app_sneeddao_frontend/src/components/Header.jsx
@@ -33,7 +33,7 @@ function Header({ showTotalValue, showSnsDropdown, onSnsChange, customLogo }) {
         if (['/dao', '/dao_info', '/rll_info', '/rll', '/products', '/partners', '/projects', '/disclaimer'].includes(path)) return 'DAO';
         if (['/hub', '/proposals', '/neurons', '/transactions', '/neuron', '/proposal', '/transaction', '/principal', '/forum', '/thread', '/post'].includes(path) || location.pathname.startsWith('/topic/')) return 'Hub';
         if (['/wallet'].includes(path)) return 'Wallet';
-        if (['/me', '/rewards', '/tips', '/posts', '/sms'].includes(path) || path.startsWith('/msg/')) return 'Me';
+        if (['/me', '/rewards', '/tips', '/posts', '/sms'].includes(path) || path.startsWith('/msg')) return 'Me';
         if (['/sneedlock', '/sneedlock_info'].includes(path)) return 'Locks';
         if (['/tools/main', '/tools/escrow', '/tools/escrow/swap'].includes(path) || location.pathname.startsWith('/tools/')) return 'Tools';
         if (['/admin'].includes(path) || location.pathname.startsWith('/admin/')) return 'Admin';
@@ -71,7 +71,7 @@ function Header({ showTotalValue, showSnsDropdown, onSnsChange, customLogo }) {
             setActiveSection('Hub');
         } else if (['/wallet'].includes(path)) {
             setActiveSection('Wallet');
-        } else if (['/me', '/rewards', '/tips', '/posts', '/sms'].includes(path) || path.startsWith('/msg/')) {
+        } else if (['/me', '/rewards', '/tips', '/posts', '/sms'].includes(path) || path.startsWith('/msg')) {
             setActiveSection('Me');
         } else if (['/sneedlock', '/sneedlock_info'].includes(path)) {
             setActiveSection('Locks');
@@ -137,9 +137,9 @@ function Header({ showTotalValue, showSnsDropdown, onSnsChange, customLogo }) {
             defaultPath: '/me',
             subMenu: [
                 { name: 'Me', path: '/me' },
+                { name: 'Messages', path: '/sms' },
                 { name: 'Posts', path: '/posts' },
                 { name: 'Tips', path: '/tips' },
-                { name: 'Messages', path: '/sms' },
                 { name: 'Rewards', path: '/rewards' }
             ]
         },
