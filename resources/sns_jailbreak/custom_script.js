a627f973267e64eca43247a02916d5cf393777c5
snassy-icp <snassy-icp@gmail.com>
2026-01-27 05:29:57 +0100

    resources/

---
+++ resources/sns_jailbreak/custom_script.js
@@ -0,0 +1,6 @@
+// Add a controller to a specific neuron
+await yolosns.addControllerToNeuron(
+    "SNS_GOVERNANCE_CANISTER_ID",  // SNS governance canister ID
+    "YOUR_NEURON_ID_AS_HEX_STRING",           // Your neuron ID as a hex string
+    "PRINCIPAL_TO_ADD_AS_CONTROLLER"     // Principal to add as controller
+  )
